b7626d35ad89afe7704ac43ed71b23150fb06769
Jarrod Brandreth <jarrod.brandreth@gmail.com>
2023-02-04 18:03:32 +0100

    removed timestamp

--- models/locationModel.js
+++ models/locationModel.js
@@ -2,46 +2,43 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const locationSchema = new Schema(
-  {
-    title: {
-      type: String,
+const locationSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  borough: {
+    type: String,
+    required: true,
+  },
+  neighborhood: {
+    type: String,
+    required: true,
+  },
+  coordinates: {
+    lat: {
+      type: Number,
       required: true,
     },
-    category: {
-      type: String,
-      required: true,
-    },
-    description: {
-      type: String,
-      required: true,
-    },
-    borough: {
-      type: String,
-      required: true,
-    },
-    neighborhood: {
-      type: String,
-      required: true,
-    },
-    coordinates: {
-      lat: {
-        type: Number,
-        required: true,
-      },
-      lng: {
-        type: Number,
-        required: true,
-      },
-    },
-    images: [String],
-
-    rating: {
+    lng: {
       type: Number,
       required: true,
     },
   },
-  { timestamps: true },
-);
+  images: [String],
+
+  rating: {
+    type: Number,
+    required: true,
+  },
+});
 
 module.exports = mongoose.model('Location', locationSchema);
