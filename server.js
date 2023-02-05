313385a2be7fb5b2da73d5377110017682af5c61
Jarrod Brandreth <jarrod.brandreth@gmail.com>
2023-02-05 09:55:10 +0100

    enable cors for client

--- server.js
+++ server.js
@@ -2,16 +2,21 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const locationRoutes = require('./routes/locations');
 const userRoutes = require('./routes/user');
 
 const app = express();
 const port = process.env.PORT;
+const corsOptions = {
+  origin: process.env.CLIENT, // allow origin client
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
 
 // middleware
 app.use(express.json());
-
+app.use(cors(corsOptions));
 app.use('/api/locations', locationRoutes);
 app.use('/api/user', userRoutes);
 
